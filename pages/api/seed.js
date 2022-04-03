a572cbf71cc367365e8c463f52df6976cd2c4d01
rakesh7305 <59904119+rakesh7305@users.noreply.github.com>
2022-04-03 00:42:15 -0400

    Update seed.js

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,5 +1,5 @@
 import nc from 'next-connect';
-import Product from '../../models/Product';
+// import Product from '../../models/Product';
 import db from '../../utils/db';
 import data from '../../utils/data';
 // import User from '../../models/User';
