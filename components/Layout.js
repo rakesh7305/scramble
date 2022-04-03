a88459f8152fffac70a2411be89180a45868c8c5
rakesh7305 <59904119+rakesh7305@users.noreply.github.com>
2022-04-03 01:00:40 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -82,43 +82,20 @@ export default function Layout({ title, description, children }) {
   // };
 
   const [categories, setCategories] = useState([]);
-  const { enqueueSnackbar } = useSnackbar();
+//   const { enqueueSnackbar } = useSnackbar();
 
-  const fetchCategories = async () => {
-    try {
-      const { data } = await axios.get(`/api/products/categories`);
-      setCategories(data);
-    } catch (err) {
-      enqueueSnackbar(getError(err), { variant: 'error' });
-    }
-  };
+//   const fetchCategories = async () => {
+//     try {
+//       const { data } = await axios.get(`/api/products/categories`);
+//       setCategories(data);
+//     } catch (err) {
+//       enqueueSnackbar(getError(err), { variant: 'error' });
+//     }
+//   };
 
-  // const [query, setQuery] = useState('');
-  // const queryChangeHandler = (e) => {
-  //   setQuery(e.target.value);
-  // };
-  // const [genderQuery, setGenderQuery] = useState('');
-  // const genderQueryChangeHandler = (e) => {
-  //   setGenderQuery(e.target.value);
-  // };
-
-  // const [ageQuery, setAgeQuery] = useState('');
-  // const ageQueryChangeHandler = (e) => {
-  //   setAgeQuery(e.target.value);
-  // };
-
-  // const submitHandler = (e) => {
-  //   e.preventDefault();
-  //   console.log(genderQuery);
-  //   console.log(ageQuery);
-  //   //router.push(`/search?query=${query}`);
-  //   router.push(`/search?gender=${genderQuery}&age=${ageQuery}`);
-
-  // };
-
-  useEffect(() => {
-    fetchCategories();
-  }, []);
+//   useEffect(() => {
+//     fetchCategories();
+//   }, []);
 
   const darkModeChangeHandler = () => {
     dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
@@ -144,55 +121,11 @@ export default function Layout({ title, description, children }) {
     Cookies.remove('paymentMethod');
     router.push('/');
   };
-  // const genders = [
-  //   {
-  //     value: 'M',
-  //     label: 'Groom (Var)',
-  //   },
-  //   {
-  //     value: 'F',
-  //     label: 'Bride (Kanya)',
-  //   },
-  // ];
-  // const age = [
-  //   {
-  //     name: '18 to 20',
-  //     value: '18-20',
-  //   },
-  //   {
-  //     name: '20 to 23',
-  //     value: '20-23',
-  //   },
-  //   {
-  //     name: '23 to 26',
-  //     value: '23-26',
-  //   },
-  //   {
-  //     name: '26 to 29',
-  //     value: '26-29',
-  //   },
-  //   {
-  //     name: '29 to 32',
-  //     value: '29-32',
-  //   },
-  //   {
-  //     name: '32 to 35',
-  //     value: '32-35',
-  //   },
-  //   {
-  //     name: '35 to 38',
-  //     value: '35-38',
-  //   },
-  //   {
-  //     name: '38 and above',
-  //     value: '38-80',
-  //   },
-  // ];
-  // console.log("in layout");
+  
   return (
     <div>
       <Head>
-        <title>{title ? `${title} - KP Vivaah` : 'KP Vivaah'}</title>
+        <title>{title ? `${title} - ELMCRABS` : 'ELMCRABS'}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
       <ThemeProvider theme={theme}>
@@ -210,7 +143,7 @@ export default function Layout({ title, description, children }) {
               </IconButton>
               <NextLink href="/" passHref>
                 <Link>
-                  <Typography className={classes.brand}>KP Vivaah</Typography>
+                  <Typography className={classes.brand}>ELMCRABS</Typography>
                 </Link>
               </NextLink>
             </Box>
@@ -335,61 +268,61 @@ export default function Layout({ title, description, children }) {
                   </Typography>
                 </Link>
               </NextLink>
-               */}
-              {userInfo ? (
-                <>
-                  <Button
-                    aria-controls="simple-menu"
-                    aria-haspopup="true"
-                    onClick={loginClickHandler}
-                    className={classes.navbarButton}
-                  >
-                    {userInfo.name}
-                  </Button>
-                  <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={loginMenuCloseHandler}
-                  >
-                    <MenuItem
-                      onClick={(e) => loginMenuCloseHandler(e, '/profile')}
-                    >
-                      Profile
-                    </MenuItem>
-                    <MenuItem
-                      onClick={(e) =>
-                        loginMenuCloseHandler(e, '/order-history')
-                      }
-                    >
-                      Order Hisotry
-                    </MenuItem>
-                    {userInfo.isAdmin && (
-                      <MenuItem
-                        onClick={(e) =>
-                          loginMenuCloseHandler(e, '/admin/dashboard')
-                        }
-                      >
-                        Admin Dashboard
-                      </MenuItem>
-                    )}
-                    <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
-                  </Menu>
-                </>
-              ) : (
-                <NextLink href="/login" passHref>
-                  <Link>
-                    <Typography component="span">Login</Typography>
-                  </Link>
-                </NextLink>
-              )}
+//                */}
+//               {userInfo ? (
+//                 <>
+//                   <Button
+//                     aria-controls="simple-menu"
+//                     aria-haspopup="true"
+//                     onClick={loginClickHandler}
+//                     className={classes.navbarButton}
+//                   >
+//                     {userInfo.name}
+//                   </Button>
+//                   <Menu
+//                     id="simple-menu"
+//                     anchorEl={anchorEl}
+//                     keepMounted
+//                     open={Boolean(anchorEl)}
+//                     onClose={loginMenuCloseHandler}
+//                   >
+//                     <MenuItem
+//                       onClick={(e) => loginMenuCloseHandler(e, '/profile')}
+//                     >
+//                       Profile
+//                     </MenuItem>
+//                     <MenuItem
+//                       onClick={(e) =>
+//                         loginMenuCloseHandler(e, '/order-history')
+//                       }
+//                     >
+//                       Order Hisotry
+//                     </MenuItem>
+//                     {userInfo.isAdmin && (
+//                       <MenuItem
+//                         onClick={(e) =>
+//                           loginMenuCloseHandler(e, '/admin/dashboard')
+//                         }
+//                       >
+//                         Admin Dashboard
+//                       </MenuItem>
+//                     )}
+//                     <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
+//                   </Menu>
+//                 </>
+//               ) : (
+//                 <NextLink href="/login" passHref>
+//                   <Link>
+//                     <Typography component="span">Login</Typography>
+//                   </Link>
+//                 </NextLink>
+//               )}
             </div>
           </Toolbar>
         </AppBar>
         <Container className={classes.main}>{children}</Container>
         <footer className={classes.footer}>
-          <Typography>All rights reserved. KP Vivaah.</Typography>
+//           <Typography>All rights reserved. ELMCRABS.</Typography>
         </footer>
       </ThemeProvider>
     </div>
