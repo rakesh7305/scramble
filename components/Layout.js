5657aeaf131d64b9bb9f93be78e1537c0d1a9521
rakesh7305 <59904119+rakesh7305@users.noreply.github.com>
2022-04-03 01:41:05 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -22,6 +22,7 @@ import {
   ListItem,
   Divider,
   ListItemText,
+  Grid,
   // InputBase,
   // FormControl,
   // InputLabel,
@@ -73,29 +74,10 @@ export default function Layout({ title, description, children }) {
   });
   const classes = useStyles();
 
-  // const [sidbarVisible, setSidebarVisible] = useState(false);
-  // const sidebarOpenHandler = () => {
-  //   setSidebarVisible(true);
-  // };
-  // const sidebarCloseHandler = () => {
-  //   setSidebarVisible(false);
-  // };
+
 
   const [categories, setCategories] = useState([]);
-//   const { enqueueSnackbar } = useSnackbar();
 
-//   const fetchCategories = async () => {
-//     try {
-//       const { data } = await axios.get(`/api/products/categories`);
-//       setCategories(data);
-//     } catch (err) {
-//       enqueueSnackbar(getError(err), { variant: 'error' });
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchCategories();
-//   }, []);
 
   const darkModeChangeHandler = () => {
     dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
@@ -106,22 +88,8 @@ export default function Layout({ title, description, children }) {
   const loginClickHandler = (e) => {
     setAnchorEl(e.currentTarget);
   };
-  const loginMenuCloseHandler = (e, redirect) => {
-    setAnchorEl(null);
-    if (redirect) {
-      router.push(redirect);
-    }
-  };
-  const logoutClickHandler = () => {
-    setAnchorEl(null);
-    dispatch({ type: 'USER_LOGOUT' });
-    Cookies.remove('userInfo');
-    Cookies.remove('cartItems');
-    Cookies.remove('shippinhAddress');
-    Cookies.remove('paymentMethod');
-    router.push('/');
-  };
-  
+
+
   return (
     <div>
       <Head>
@@ -132,197 +100,31 @@ export default function Layout({ title, description, children }) {
         <CssBaseline />
         <AppBar position="static" className={classes.navbar}>
           <Toolbar className={classes.toolbar}>
-            <Box display="flex" alignItems="center">
-              <IconButton
-                edge="start"
-                aria-label="open drawer"
-                //onClick={sidebarOpenHandler}
-                className={classes.menuButton}
-              >
-                <MenuIcon className={classes.navbarButton} />
-              </IconButton>
-              <NextLink href="/" passHref>
-                <Link>
-                  <Typography className={classes.brand}>ELMCRABS</Typography>
-                </Link>
-              </NextLink>
-            </Box>
-            <Drawer
-              anchor="left"
-                // open={sidbarVisible}
-                // onClose={sidebarCloseHandler}
+        
+
+            <Grid container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
             >
-              <List>
-                <ListItem>
-                  <Box
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="space-between"
-                  >
-                    <Typography>Shopping by category</Typography>
-                    <IconButton
-                      aria-label="close"
-                      // onClick={sidebarCloseHandler}
-                    >
-                      <CancelIcon />
-                    </IconButton>
-                  </Box>
-                </ListItem>
-                <Divider light />
-                {categories.map((category) => (
-                  <NextLink
-                    key={category}
-                    href={`/search?category=${category}`}
-                    passHref
-                  >
-                    <ListItem
-                      button
-                      component="a"
-                      // onClick={sidebarCloseHandler}
-                    >
-                      <ListItemText primary={category}></ListItemText>
-                    </ListItem>
-                  </NextLink>
-                ))}
-              </List>
-            </Drawer>
+              <Grid item
+              >
+                <Typography className={classes.brand}>ELMCRABS</Typography>
+              </Grid>
+            </Grid>
 
-            {/* <div className={classes.searchSection}>
 
-              <form onSubmit={submitHandler} className={classes.searchForm}>
-                
-                <Box
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                  // sx={{ m: 1, minWidth: 400 }}
-                >
-                  
-                  <TextField
-                    id="outlined-select-currency"
-                    select
-                    label="Gender"
-                    className={classes.searchInput}
-                    onChange={genderQueryChangeHandler}
-                    // fullWidth
-                    size="small"
-                  >
-                    <MenuItem value="">
-                      <em>All</em>
-                    </MenuItem>
-                    {genders.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                  <TextField
-                    id="outlined-select-currency"
-                    select
-                    label="Age"
-                    className={classes.searchInput}
-                    onChange={ageQueryChangeHandler}
-                    // fullWidth
-                    size="small"
-                  >
-                    <MenuItem value="">
-                      <em>All</em>
-                    </MenuItem>
-                    {age.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.name}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                  
-                  <IconButton
-                    type="submit"
-                    className={classes.iconButton}
-                    aria-label="search"
-                  >
-                    <SearchIcon />
-                  </IconButton>
-                </Box>
 
-              </form>
-            </div> */}
             <div>
               <Switch
                 checked={darkMode}
                 onChange={darkModeChangeHandler}
               ></Switch>
-              
-              {/* <NextLink href="/cart" passHref>
-                <Link>
-                  <Typography component="span">
-                    {cart.cartItems.length > 0 ? (
-                      <Badge
-                        color="secondary"
-                        badgeContent={cart.cartItems.length}
-                      >
-                        Cart
-                      </Badge>
-                    ) : (
-                      'Cart'
-                    )}
-                  </Typography>
-                </Link>
-              </NextLink>
-//                */}
-//               {userInfo ? (
-//                 <>
-//                   <Button
-//                     aria-controls="simple-menu"
-//                     aria-haspopup="true"
-//                     onClick={loginClickHandler}
-//                     className={classes.navbarButton}
-//                   >
-//                     {userInfo.name}
-//                   </Button>
-//                   <Menu
-//                     id="simple-menu"
-//                     anchorEl={anchorEl}
-//                     keepMounted
-//                     open={Boolean(anchorEl)}
-//                     onClose={loginMenuCloseHandler}
-//                   >
-//                     <MenuItem
-//                       onClick={(e) => loginMenuCloseHandler(e, '/profile')}
-//                     >
-//                       Profile
-//                     </MenuItem>
-//                     <MenuItem
-//                       onClick={(e) =>
-//                         loginMenuCloseHandler(e, '/order-history')
-//                       }
-//                     >
-//                       Order Hisotry
-//                     </MenuItem>
-//                     {userInfo.isAdmin && (
-//                       <MenuItem
-//                         onClick={(e) =>
-//                           loginMenuCloseHandler(e, '/admin/dashboard')
-//                         }
-//                       >
-//                         Admin Dashboard
-//                       </MenuItem>
-//                     )}
-//                     <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
-//                   </Menu>
-//                 </>
-//               ) : (
-//                 <NextLink href="/login" passHref>
-//                   <Link>
-//                     <Typography component="span">Login</Typography>
-//                   </Link>
-//                 </NextLink>
-//               )}
             </div>
           </Toolbar>
         </AppBar>
         <Container className={classes.main}>{children}</Container>
         <footer className={classes.footer}>
-//           <Typography>All rights reserved. ELMCRABS.</Typography>
         </footer>
       </ThemeProvider>
     </div>
