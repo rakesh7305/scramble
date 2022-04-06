2bdb40df3cc2a77a5e34527b6039093d69f0b5e0
rakesh7305 <59904119+rakesh7305@users.noreply.github.com>
2022-04-05 22:51:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,26 @@
 import {
-  // List,
-  // ListItem,
   Typography,
   TextField,
-  // Button,
-  // Link,
+  Input,
   Grid,
   Box,
   IconButton,
+  Button,
 } from '@material-ui/core';
-// import axios from 'axios';
-// import { useRouter } from 'next/router';
-// import NextLink from 'next/link';
 import React, { useContext, useEffect, useState, useRef } from 'react';
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
-// import useStyles from '../utils/styles';
-// import Cookies from 'js-cookie';
-// import { useForm } from 'react-hook-form';
+
 import { useSnackbar } from 'notistack';
-// import { getError } from '../utils/error';
-// import ReactDOM from "react-dom";
+
 import db from '../utils/db';
 import ScrambleGame from '../models/ScrambleGame';
-//import { useTimer } from "../components/useTimer";
 import SuccessDialog from "../components/SuccessDialog";
 import StatisticDialog from "../components/StatisticDialog";
 import InsertChartOutlinedIcon from '@material-ui/icons/InsertChartOutlined';
+import BackspaceIcon from '@mui/icons-material/Backspace';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+
 // import CancelIcon from '@material-ui/icons/Cancel';
 import moment from 'moment';
 
@@ -34,35 +28,24 @@ import moment from 'moment';
 
 
 export default function Scramble(props) {
-  // const {
-  //   handleSubmit,
-  //   control,
-  //   formState: { errors },
-  // } = useForm();
+
   const { enqueueSnackbar } = useSnackbar();
-  // const router = useRouter();
-  // const { redirect } = router.query;
+
   const { dispatch } = useContext(Store);
-  // const { scrambleStat } = state;
   const [showModal, setShowModal] = useState(false);
-  const [showStatModal, setShowStatisticModal] = useState(false);
+  // const [showStatModal, setShowStatisticModal] = useState(false);
   const [seconds, setSeconds] = useState(0);
-  const [timer, setTimer] = useState();
+  //const [timer, setTimer] = useState();
 
-  //const { seconds, start, stop } = useTimer();
-
-
+  let timer = null;
   useEffect(() => {
-    // if (userInfo) {
-    //   router.push('/');
-    // }
-    //start();
     document.getElementById("0").focus();
 
-    const timer = setInterval(() => {
+    timer = setInterval(() => {
       setSeconds(prevSeconds => prevSeconds + 1);
     }, 1000);
-    setTimer(timer);
+    //setTimer(timer);
+
     return () => {
       clearInterval(timer);
     };
@@ -76,46 +59,34 @@ export default function Scramble(props) {
   // const classes = useStyles();
 
 
-  // const INITIAL_ANS = Array.from({ length: word.length }, () => "");
-  //const [ansewer, setAnswer] = useState();
-  // const [rightAnswer, setrightAnswer] = useState(word.join(""));
   const rightAnswer = word.join("");
-  //const [allValues, setAllValues] = useState(INITIAL_ANS);
   const allValues = useRef([]);
-  // const clockRef = useRef();
 
-  // const [newWord, setNewWord] = useState(Array.from(scrambleGame.scrambledClue));
   const newWord = Array.from(scrambleGame.scrambledClue);
-
-  // console.log("All Values:" + allValues.current);
-  // console.log("newWord:" + newWord);
 
   var shuffledWord_init = [];
   for (var i = 0; i < newWord.length; i++) {
     var item = { "id": 100 + i, "letter": newWord[i], "checked": false, "responseLetterId": -1 };
     shuffledWord_init.push(item);
   }
-  // const [shuffledWord, setShuffledWord] = useState(shuffledWord_init);
   const shuffledWord = shuffledWord_init;
 
-  // console.log("JSON=" + JSON.stringify(shuffledWord));
 
   const saveStat = () => {
     const data = {
-      gameDate: moment().format("DD-MMM HH:mm"), //Date.now(),
+      gameDate: scrambleGame.gameNo, //moment().format("DD-MMM HH:mm"), //Date.now(),
       time: seconds,
     };
     dispatch({ type: 'SCRAMBLE_ADD_STAT', payload: data });
-    //Cookies.set('scrambleStat', data);
   };
 
   const successDialogHandler = () => {
     setShowModal(false);
   };
 
-  const statisticDialogHandler = () => {
-    setShowStatisticModal(false);
-  };
+  // const statisticDialogHandler = () => {
+  //   setShowStatisticModal(false);
+  // };
 
   function disableAnswer() {
 
@@ -126,7 +97,6 @@ export default function Scramble(props) {
   }
 
   function checkLetter(updatedAnswer) {
-    // console.log("in checkletter " + updatedAnswer);
     shuffledWord.forEach((item) => {
       item.checked = false;
       item.responseLetterId = -1;
@@ -140,7 +110,6 @@ export default function Scramble(props) {
           item.checked = true;
           item.responseLetterId = val;
           document.getElementById((item.id).toString()).style.background = '#00e600';
-          //document.getElementById((item.id).toString()).style.background = '#04470a';
           document.getElementById((item.id).toString()).style.color = 'white';
         }
       }
@@ -148,8 +117,7 @@ export default function Scramble(props) {
 
   }
 
-  function updateAllValues(val, index) {
-    val = index < 0 ? "" : "";
+  function updateAllValues() {
 
     const userAnswer = Array.from({ length: allValues.current.length }, () => "");
 
@@ -159,28 +127,32 @@ export default function Scramble(props) {
     );
 
     const updatedAnswer = userAnswer;
-    // const updatedAnswer = [...allValues];
-    // updatedAnswer[index] = val;
 
-    //setAllValues(updatedAnswer);
-    //console.log("before calling checkletter:" + updatedAnswer);
     checkLetter(updatedAnswer);
-    //console.log("after calling checkletter:" + allValues);
   }
+
+  // function updateAllValues(val, index) {
+  //   val = index < 0 ? "" : "";
+
+  //   const userAnswer = Array.from({ length: allValues.current.length }, () => "");
+
+  //   allValues.current.map((item, index) => (
+  //     userAnswer[index] = (item.value).toUpperCase()
+  //   )
+  //   );
+
+  //   const updatedAnswer = userAnswer;
+
+  //   checkLetter(updatedAnswer);
+  // }
+
   function checkAnswer() {
-    //const userAnswer = allValues.join("");
-    //const userAnswer = allValues.current.value.join("");
     const userAnswer = Array.from({ length: allValues.current.length }, () => "");
 
-    //let userAnswer = "";
     allValues.current.map((item, index) => (userAnswer[index] = (item.value).toUpperCase()));
     const userResponse = userAnswer.join("");
-    // console.log(word);
-    // console.log(rightAnswer);
-    // console.log(userResponse);
 
     if (rightAnswer.localeCompare(userResponse.toUpperCase()) === 0) {
-      // stop();
       clearInterval(timer);
       disableAnswer();
       const answerTime = (Math.floor(seconds / 60) < 10 ? "0" + (Math.floor(seconds / 60)).toString() : Math.floor(seconds / 60)) + ":" + (seconds % 60 < 10 ? "0" + (seconds % 60).toString() : seconds % 60);
@@ -193,27 +165,99 @@ export default function Scramble(props) {
     } else {
       enqueueSnackbar("wrong", { variant: 'error' });
     }
-    //console.log("After update all Values:" + allValues);
   }
 
+  function clickBackspace() {
+    let index = 0;
+    let found = false;
+    allValues.current.forEach((item) => {
+      if (item.value === "" && !found && index > 0) {
+        found = true;
+        allValues.current[index-1].value = '';
+        updateAllValues();
+        allValues.current[index-1].focus();
+        // e.target.style.background = '#00e600';
+        // e.target.style.color = 'white';
+      }
+      else if(index === allValues.current.length - 1 && !found) {
+        allValues.current[allValues.current.length - 1 ].value = '';
+        updateAllValues();
+        allValues.current[allValues.current.length - 1 ].focus();
+      }
+      index++;
+    });
+  };
 
 
   const ansChangeHandler = (e) => {
     const letterValue = (e.target.value).toUpperCase();
-    // console.log("in Handler new = " + letterValue);
 
     const currentId = parseInt(e.target.id);
 
     if (!letterValue.match(/[A-Z]/)) {
       e.target.value = "";
-      updateAllValues(letterValue, currentId);
+      // updateAllValues(letterValue, currentId);
+      updateAllValues();
     } else {
-      //checkLetter(letterValue);
       if (currentId < word.length - 1) {
         document.getElementById((currentId + 1).toString()).focus();
       }
-      updateAllValues(letterValue, currentId);
+      // updateAllValues(letterValue, currentId);
+      updateAllValues();
     }
+  };
+
+  const onClickShuffledWordHandler = (e) => {
+    const letterValue = (e.target.value).toUpperCase();
+    console.log("on click event: " + letterValue);
+
+    // allValues.current.map((item, index) => (
+    //   if (item.value === "") {
+    //     document.getElementById((index).toString()).innerText = letterValue;
+    //   }
+    // )
+    // )
+    let index = 0;
+    let found = false;
+
+    allValues.current.forEach((item) => {
+      if (item.value === "" && !found) {
+        item.value = letterValue;
+        e.target.style.background = '#00e600';
+        e.target.style.color = 'white';
+        found = true;
+      }
+      index++;
+
+    });
+
+  };
+
+  const onKeyUpHandler = (e) => {
+    // console.log("on key up event: " + event.which);
+    if (event.which === 8) {
+      const currentId = event.target.id;
+      //checkLetter();
+      event.target.value = "";
+      const prevId = (currentId - 1).toString();
+      if (currentId > 0) {
+        //allValues.current[index-1].focus()
+        document.getElementById(prevId).focus();
+        //document.getElementById(prevId).select();
+        // updateAllValues("", currentId);
+        updateAllValues();
+
+      } else if (currentId === 0) {
+        console.log("backspae " + currentId + " " + allValues.current[currentId].value)
+        // updateAllValues("", currentId);
+        updateAllValues();
+
+      }
+    }
+    else if (event.which === 13) {
+      checkAnswer();
+    }
+
   };
 
 
@@ -241,20 +285,18 @@ export default function Scramble(props) {
       <Grid container justifyContent="center"
         alignItems="center"
         style={{ minHeight: "70vh" }}
-
       >
         <div>
           <SuccessDialog
-            // onClose={() => setShowModal(false)}
             show={showModal}
             handler={successDialogHandler}
           >
           </SuccessDialog>
-          <StatisticDialog
+          {/* <StatisticDialog
             show={showStatModal}
             handler={statisticDialogHandler}
           >
-          </StatisticDialog>
+          </StatisticDialog> */}
         </div>
         <Grid container
           // direction="row"
@@ -263,15 +305,15 @@ export default function Scramble(props) {
           alignItems="center"
         // style={{ minHeight: "5vh" }}
         >
-          {/* <Grid item xs={12} spacing={8}> */}
-          <Grid item>
+          {/* <Grid item>
             <IconButton
               aria-label="stat"
               onClick={() => setShowStatisticModal(true)}
             >
               <InsertChartOutlinedIcon />
             </IconButton>
-          </Grid>
+          </Grid> */}
+   
         </Grid>
         <Box
           component="form"
@@ -283,42 +325,46 @@ export default function Scramble(props) {
           autoComplete="off"
           bgcolor="white"
         >
-
           <Grid container spacing={2}
             direction="row"
-            wrap="nowrap"
             justifyContent="center"
             alignItems="center"
             textAlign="center" >
             <Grid item >
               {shuffledWord &&
-                shuffledWord.map((item) => (
-                  <TextField
-                    id={(item.id).toString()}
-                    key={item.id}
-                    defaultValue={item.letter}
+                shuffledWord.map((letter, index) => (
+                  <Input
+                    id={index.toString()}
+                    key={index}
+                    //defaultValue=""
+                    inputRef={el => allValues.current[index] = el}
+                    //ref={allValues[index]}
                     variant="outlined"
+                    autoFocus
+                    onChange={ansChangeHandler}
+                    onKeyUp={onKeyUpHandler}
                     inputProps={{
-                      readOnly: true,
+                      readOnly: false,
+                      maxLength: 1,
                       style: {
-                        width: "1.5ch",
-                        wrap: "nowrap",
+                        width: "2ch",
                         fontSize: 30,
-                        // background: "white",
                         background: "lightgrey",
-                        color: "black",
+                        color: "#470404",
                         fontWeight: "bold",
                         textAlign: "center",
-                        border: "6px solid white",
-                        ml: 20,
+                        // pl: 20,
+                        textTransform: "uppercase",
+                        pattern: "[a-z]",
+                        border: "2px solid white",
                       }
                     }}
                   />
-
                 ))}
 
             </Grid>
           </Grid>
+
           <Grid container
             direction="column"
             justifyContent="center"
@@ -332,60 +378,37 @@ export default function Scramble(props) {
           </Grid>
           <Grid container spacing={2}
             direction="row"
+            // width="100%"
             justifyContent="center"
             alignItems="center"
             textAlign="center" >
-            <Grid item >
+            <Grid item>
               {shuffledWord &&
-                shuffledWord.map((letter, index) => (
-                  <TextField
-                    id={index.toString()}
-                    key={index}
-                    //defaultValue=""
-                    inputRef={el => allValues.current[index] = el}
-                    //ref={allValues[index]}
+                shuffledWord.map((item) => (
+                  <Input
+                    id={(item.id).toString()}
+                    key={item.id}
+                    defaultValue={item.letter}
                     variant="outlined"
-                    autoFocus
-                    onChange={ansChangeHandler}
-                    onKeyUp={() => {
-                      // console.log("on key up event: " + event.which);
-                      if (event.which === 8) {
-                        const currentId = event.target.id;
-                        //checkLetter();
-                        event.target.value = "";
-                        const prevId = (currentId - 1).toString();
-                        if (currentId > 0) {
-                          //allValues.current[index-1].focus()
-                          document.getElementById(prevId).focus();
-                          //document.getElementById(prevId).select();
-                          updateAllValues("", currentId);
-                        } else if (currentId === 0) {
-                          console.log("backspae " + currentId + " " + allValues.current[currentId].value)
-                          updateAllValues("", currentId);
-                        }
-                      }
-                      else if (event.which === 13) {
-                        checkAnswer();
-                      }
-                    }}
+                    onClick={onClickShuffledWordHandler}
                     inputProps={{
-                      readOnly: false,
-                      maxLength: 1,
+                      readOnly: true,
                       style: {
-                        width: "1.5ch",
+                        width: "2ch",
                         wrap: "nowrap",
                         fontSize: 30,
+                        // background: "white",
                         background: "lightgrey",
-                        color: "#470404",
+                        color: "black",
                         fontWeight: "bold",
                         textAlign: "center",
-                        pl: 20,
-                        textTransform: "uppercase",
-                        pattern: "[a-z]",
-                        border: "6px solid white",
+                        border: "1px solid white",
+                        // m: 2,
+                        // p: 2,
                       }
                     }}
                   />
+
                 ))}
 
             </Grid>
@@ -406,6 +429,27 @@ export default function Scramble(props) {
               <Typography id='message' variant='h6' color='secondary' fontFamily='Segoe UI'></Typography>
             </Grid>
           </Grid>
+          <Grid container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            style={{ minHeight: "9vh" }}
+          >
+            <Grid item xs style={{ display: "flex", justifyContent: "flex-start" }}
+              onClick={checkAnswer}
+            >
+              <Button variant="contained" color="secondary" size="small" startIcon={<KeyboardReturnIcon />}>
+                ENTER
+              </Button>
+            </Grid>
+            <Grid item xs style={{ display: "flex", justifyContent: "flex-end" }}
+              onClick={clickBackspace}
+            >
+              <Button variant="contained" color="secondary" size="small" startIcon={<BackspaceIcon />}>
+                BKSP
+              </Button>
+            </Grid>
+          </Grid>
         </Box>
       </Grid>
     </Layout>
@@ -413,7 +457,8 @@ export default function Scramble(props) {
 }
 export async function getServerSideProps() {
   //const { params } = context;
-  const gameDate = new Date("2022-03-14");
+  //const gameDate = new Date("2022-03-14");
+  const gameDate = moment().format("YYYY-MM-DD");
 
   await db.connect();
   const scrambleGame1 = await ScrambleGame.findOne({ gameDate }).lean();
